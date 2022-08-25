c102ba1581fa78e5d7fdf3c1b81684ad5786170a
Ari Rocha <arizinhorocha3@gmail.com>
2022-08-25 15:26:15 -0300

    Added image to domain whitelist

--- next.config.js
+++ next.config.js
@@ -2,14 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
+  images: {
+    domains: ['sm.ign.com']
+  }
 };
 
 module.exports = nextConfig;
