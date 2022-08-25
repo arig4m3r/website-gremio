b810e5a8aa683e716eaf5039719675bb9da5b8e4
Ari Rocha <arizinhorocha3@gmail.com>
2022-08-25 13:28:03 -0300

    Updated divider as a React Component

---
+++ components/Divider.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Divider() {
+  return (
+    <div className="divide-x-2 h-[1.5px] bg-gray-900" />
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Divider from "../components/Divider";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import HeroCard from "../components/HeroCard";
@@ -12,11 +13,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Grêmio - Guimaraes Rosa</title>
       </Head>
+      
       <Navbar />
       <HeroCard />
-      <div className="divide-x-2 h-[1.5px] bg-gray-900" />
+      <Divider />
       <Features />
-      <div className="divide-x-2 h-[1.5px] bg-gray-900" />
+      <Divider />
       <Footer />
     </div>
   );
