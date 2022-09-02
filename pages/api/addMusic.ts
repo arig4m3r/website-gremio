3d94ec572fe2ef4437c387bd1ac781852c2bb638
Ari Rocha <arizinhorocha3@gmail.com>
2022-09-01 21:58:20 -0300

    Updated pusher config (Leaked before)

--- pages/api/addMusic.ts
+++ pages/api/addMusic.ts
@@ -1,22 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Pusher from 'pusher'
+import type { NextApiRequest, NextApiResponse } from "next";
+import Pusher from "pusher";
 
 const pusher = new Pusher({
-    appId: '1471045',
-    key: '38000fe8ad1be55a3924',
-    secret: '179bb0ac718045bf0351',
-    cluster: 'sa1',
-    useTLS: true
-})
+  appId: process.env.PUSHER_APPID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
+  useTLS: true,
+});
 
 type Data = {
-    name: string
-}
+  name: string;
+};
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-    pusher.trigger('gremio-website', 'music-request', req.body)
+  pusher.trigger("gremio-website", "music-request", req.body);
 }
